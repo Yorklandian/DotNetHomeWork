24e4f3c9e08b7d5e6548a57d8778ecb9f382e61c
Yorklandian <79745101+Yorklandian@users.noreply.github.com>
2021-03-22 17:27:40 +0800

    Add files via upload

---
+++ homework4/question1/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace question1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericList<double> List = new GenericList<double>();
+            for(int i = 0; i < 10; i++)
+            {
+                List.Add(i);
+            }
+            List.ForEach(n => Console.WriteLine(n + " "));
+            double max=0, min=0,sum=0;
+            List.ForEach(n =>
+            {
+                max = (n >= max) ? n : max;
+                min = (n <= min) ? n : min;
+                sum += n;
+            });
+            Console.WriteLine("The max is " + max + "; the min is " + min + " ;the sum is:" + sum);
+        }
+    }
+    public class Node<T>
+    {
+        public Node<T> Next { get; set; }
+        public T Data { get; set; }
+        public Node(T t)
+        {
+            Next = null;
+            Data = t;
+        }
+    }
+    public class GenericList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+        public GenericList()
+        {
+            tail = head = null;
+        }
+        public Node<T> Head
+        {
+            get => head;
+        }
+        public void Add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+            if (tail == null)
+            {
+                head = tail = n;
+            }
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+        }
+        public void ForEach(Action<T> action)
+        {
+            for (Node<T> n = head; n != null; n = n.Next)
+            {
+                action(n.Data);
+            }
+        }
+    }
+}
