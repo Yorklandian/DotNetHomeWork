e6c6a453fdef8a31b6b902b40fe244da54add5e4
Yorklandian <79745101+Yorklandian@users.noreply.github.com>
2021-04-29 15:29:23 +0800

    Add files via upload

---
+++ homework9/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace homework9
+{
+    public partial class Form1 : Form
+    {
+        SimpleCrawler crawler = new SimpleCrawler();
+        public Form1()
+        {
+            InitializeComponent();           
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Button button = sender as Button;
+            try
+            {
+                textBox2.Text = null;
+                textBox3.Text = null;
+                button.Enabled = false;
+                button.Text = "正在爬";
+                foreach (string res in crawler.Crawl(textBox1.Text))
+                {
+                    textBox2.Text += res;
+                    textBox2.ScrollToCaret();
+                }
+            }
+            catch(Exception ex)
+            {
+                textBox3.Text = ex.Message;
+            }
+            finally
+            {
+                button.Enabled = true;
+                button.Text = "爬行";
+            }
+        }
+    }
+}
---
+++ homework9/SimpleCrawler.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace homework9
+{
+    public class SimpleCrawler
+    {
+        private readonly Dictionary<Uri, bool> urls = new Dictionary<Uri, bool>();
+        private int count = 0;
+        readonly Regex regex = new Regex(@"(?:href|HREF)\s*=\s*[""']([^""']+\.(?:html|htm|aspx|jsp))[""']", RegexOptions.Compiled);
+        static Uri current;
+        public IEnumerable<string> Crawl(string startUrl)
+        {          
+                urls.Add(new Uri(startUrl), false);
+                yield return "开始爬行.... \r\n";
+                while (true)
+                {
+                    current = null;
+                    foreach (Uri url in urls.Keys)
+                    {
+                        if (!urls[url])
+                        {
+                            current = url;
+                        }
+                    }
+                    if (current == null || count > 10)
+                    {
+                        break;
+                    }
+                    yield return "爬行" + current + "页面...\r\n";
+                    string message = null ;
+                    
+                        string html = DownLoad(current);
+                        urls[current] = true;
+                        count++;
+                        Parse(html);                                       
+                }
+            yield return "爬行结束\r\n";
+            urls.Clear();
+                count = 0;
+                   
+        }
+
+        public string DownLoad(Uri url)
+        {
+            try
+            {
+                WebClient webClient = new WebClient();
+                webClient.Encoding = Encoding.UTF8;
+                string html = webClient.DownloadString(url);
+                string fileName = url.AbsoluteUri.Substring(url.AbsoluteUri.LastIndexOf('/') + 1);
+                if (fileName.Equals(""))
+                {
+                    fileName = "./index.html";
+                }
+
+                File.WriteAllText(fileName, html, Encoding.UTF8);
+                return html;
+            }
+            catch(Exception ex)
+            {
+                return ex.Message;
+            } 
+        }
+
+        private void Parse(string html)
+        {
+            MatchCollection matches = regex.Matches(html);
+            foreach (Match match in matches)
+            {
+                Uri strRef = new Uri(current, match.Groups[1].Value);
+                if (!urls.ContainsKey(strRef))
+                {
+                    urls.Add(strRef, false);
+                }
+            }
+        }
+    }
+}
