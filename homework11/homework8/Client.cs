40ff81d3713d1f85b24255938505457191c22541
Yorklandian <79745101+Yorklandian@users.noreply.github.com>
2021-06-20 22:29:52 +0800

    Add files via upload

---
+++ homework11/homework8/Client.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace homework8
+{
+    public class Client
+    {
+        public string ClientName
+        {
+            get;set;
+        }
+        public string Address
+        {
+            get;set;
+        }
+        public Client() { }
+        public Client(string name,string address)
+        {
+            ClientName = name;
+            Address = address;
+        }
+        public override string ToString()
+        {
+            return $"Client name: {ClientName}, address: {Address}";
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+            Client other = (Client)obj;
+            return ClientName == other.ClientName && Address == other.Address;
+        }
+
+        public override int GetHashCode()
+        {
+            int hashCode = 664343032;
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(ClientName);
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Address);
+            return hashCode;
+        }
+    }
+}
